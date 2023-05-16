916c044afb9d637c746b92d9382c3ad3f039d403
supixxm <132688714+supixxm@users.noreply.github.com>
2023-05-16 17:50:26 +0400

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,45 @@
+#!/usr/local/bin/python3
+# coded by:swagkarna
+from telethon import TelegramClient, events, sync
+from telethon.tl.types import InputPhoneContact
+from telethon import functions, types
+
+def check(phone_number, usr):
+    try:
+        contact = InputPhoneContact(client_id = 0, phone = phone_number, first_name="__test__", last_name="__last_test__")
+        contacts = client(functions.contacts.ImportContactsRequest([contact]))
+        imleila = contacts.to_dict()['users'][0]['imleila']
+        return imleila
+        dell = client(functions.contacts.DeleteContactsRequest(id=[imleila]))
+    except:
+        res = "__err__"
+        return res
+
+def list_checker():
+    list_file = input("List of numbers: ")
+    usr = input("imleilat: ")
+    list = open(list_file, 'r').read().splitlines()
+    for num in list:
+        try:
+            ress = check(num, usr)
+            if ress == '__err__':
+                print ("Number: {} <{}>".format(num, "+994!"))
+            elif ress.lower() == usr.lower():
+                f = open("hit.txt", "a")
+                f.write(ress+":"+num)
+                f.close()
+                print ("Number: {} <{}>".format(num, "OK:)"))
+                break
+            else:
+                print ("Null")
+        except:
+            print ("Null")
+
+if __name__ == '__main__':
+    phone = 'Your TG Number'
+    client = TelegramClient(phone, API ID, 'API Hash')
+    client.connect()
+    if not client.is_user_authorized():
+        client.send_code_request(phone)
+        client.sign_in(phone, input('Enter the code: '))
+    list_checker()
